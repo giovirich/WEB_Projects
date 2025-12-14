b33e80474bd77ea81c8be8a632a609cf6832afee
giovirich <75053383+giovirich@users.noreply.github.com>
2025-12-14 10:02:49 -0500

    Delete WGU_personal_portfolio directory

--- WGU_personal_portfolio/Function.js
+++
@@ -1,7 +0,0 @@
-function showDates(){
-    let hiddenDates = document.querySelectorAll(".dates0");
-    hiddenDates.forEach(item =>{
-        item.classList.toggle("show")
-        console.log("Dates are now visible");
-    })
-}
